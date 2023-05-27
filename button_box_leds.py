6b593eb8325d4f12ec124a951b195c22ccdda60f
imadethis-smile <trentshapiro@gmail.com>
2023-05-27 11:21:02 -0500

    reset at midnight
    reset at midnight

--- button_box_leds.py
+++ button_box_leds.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import gc
 import time
+import secrets
 
 # enable garbage collection
 gc.enable()
@@ -12,9 +13,27 @@ button_default_value = 1
 board_led = Pin('LED', Pin.OUT)
 board_led.off()
 
+#start timers
+current_time = time.localtime()
+timer = time.time()
+reset_flag = 0
 
 while True:
     if any([button.value()!=button_default_value for button in buttons]):
         high_buttons = [idx for idx,button in enumerate(buttons) if button.value()!=button_default_value]
         [leds[button].value(1-leds[button].value()) for button in high_buttons]
         time.sleep(0.25)
+    
+    # Check every 5 minutes
+    if(time.time() - timer >= 300):
+        current_time = time.localtime()
+        timer = time.time()
+        # Reset at 1am, flag for if interval is hit twice
+        if current_time[3] == 1 and current_time[4] > 0 and current_time[4] < 10 and reset_flag == 0:
+            [led.value(0) for led in leds]
+            reset_flag = 1
+        
+        reset_flag = 0
+
+
+
